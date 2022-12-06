54241462c5eaad891e05d5accb0c70a945b1a5d1
cdhenaoh <91096631+UlisesBarroso@users.noreply.github.com>
2022-12-05 20:22:45 -0600

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
   
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-//use mikehaertl\wkhtmlto\Pdf;
-//use Barryvdh\DomPDF\Facade\Pdf as PDF;
+
 
 class PDFController extends Controller
 {
@@ -24,44 +23,7 @@ class PDFController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    /*
-    public function download()
-    {
 
-        $pdf = PDF::loadView('chart', [
-            		'title' => 'Frameworks',
-            		'description' => 'This is an example for project elecciones ITVO',
-            		'footer' => 'by ITVO'
-            	]);
-        
-        
-        return $pdf->download('frameworks.pdf');
-    }
-    */
-    /*
-    public function download() 
-    {
-        $render = view('chart')->render();
-        $pdf = new Pdf;
-        $options=['javascript-delay' => 5000,
-        'isJavascriptEnabled'=>true,
-
-        ];
-        try {
-            //$pdf->addPage(public_path('test.html'));
-            $pdf->addPage($render);
-            $pdf->setOptions($options);
-            $success = $pdf->saveAs(public_path('pdf/frameworks.pdf'));
-        }catch(\Exception $ex){
-            echo $pdf->getError(); exit;
-        }
-        if (!$success){
-            $message = $pdf->getError();
-            return view('message',compact('message','success'));
-        }else {
-            return response()->download(public_path('pdf/frameworks.pdf'));
-        }
-    }*/
     public function download(){
         $pdf = \PDF::loadView('chart');
         $pdf->setOption('enable-javascript', true);
